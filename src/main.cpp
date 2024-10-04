25e1e8a15a667321047ed484a5bcb0c0906b568b
Hjalram <jolshjalram@gmail.com>
2024-10-04 18:19:13 +0200

    basic window and makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include "raylib/raylib.h"
 
 int main(void) {
+    int WIDTH = 1000;
+    int HEIGHT = 700;
+
+    InitWindow(WIDTH, HEIGHT, "Yay!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground({230, 230, 230, 255});
+
+            DrawRectangle(100, 100, 200, 200, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
